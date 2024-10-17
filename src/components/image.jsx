5f8ca7a9489280c921163f673ea1d50de08b2711
Aishling13 <chloe.lodge@outlook.com>
2024-10-17 11:34:47 +0100

    Added interactivity & updated css

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,13 +1,31 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
-export function Image({ imageUrl, size, altText }) {
+export const Image = ({ imageUrl, size, altText }) => {
+  const [liked, setLiked] = useState(false);
+
+  const handleHeartClick = () => {
+    setLiked(!liked);
+  };
+
   return (
-    <img 
-      className="image" 
-      src={imageUrl}
-      alt={altText}
-      width={size}
-    />
+    <div className="image-container">
+      <img 
+        className="image" 
+        src={imageUrl}
+        alt={altText}
+        width={size}
+      />
+      <div className="overlay">
+        <button 
+        className={`icon ${liked ? 'liked' : ''}`} 
+        onClick={handleHeartClick}
+        aria-label={liked ? 'Unlike this image' : 'Like this image'}  
+        >
+          <i className="fas fa-heart"></i>
+        </button>
+      </div>
+    </div>
   );
 }
 
